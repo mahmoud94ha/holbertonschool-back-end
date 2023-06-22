416d12030a2d726ab52c394b3b691fbc9cf51e2e
mahmoud94ha <mahmoudhannani3@gmail.com>
2023-06-23 00:45:21 +0100

    up

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,42 +1,42 @@
 #!/usr/bin/python3
-"""
-Module 0-gather_data_from_an_API
-"""
+"""api"""
+import sys
 import requests
-from sys import argv
 
 
-def first_line(id):
-    id = eval(id)
-    if type(id) != int:
-        raise TypeError("The data entered is not an integer type")
+def get_employee_todo_progress(employee_id):
+    """func"""
+    base_url = 'https://jsonplaceholder.typicode.com'
+    employee_url = f'{base_url}/users/{employee_id}'
+    todos_url = f'{base_url}/todos?userId={employee_id}'
 
-    users_url = "https://jsonplaceholder.typicode.com/users/"
-    todos_url = "https://jsonplaceholder.typicode.com/todos/"
+    try:
+        employee_response = requests.get(employee_url)
+        todos_response = requests.get(todos_url)
+        employee_response.raise_for_status()
+        todos_response.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        print(f"Error: {err}")
+        return
 
-    response2 = requests.get(todos_url)
-    response = requests.get(users_url)
+    employee_data = employee_response.json()
+    todos_data = todos_response.json()
 
-    if (response.status_code and response2.status_code) == 200:
-        response = requests.get(users_url).json()
-        response2 = requests.get(todos_url).json()
+    employee_name = employee_data['name']
+    completed_tasks = \
+        [task['title'] for task in todos_data if task['completed']]
+    number_of_done_tasks = len(completed_tasks)
+    total_number_of_tasks = len(todos_data)
 
-        k = 0
-        ls = []
-        for j in range(len(response2)):
-            if response2[j]['userId'] == response[id-1]['id']\
-                    and response2[j]['completed'] is True:
-                k += 1
-                ls.append(response2[j]['title'])
-
-        for i in range(len(response)):
-            if response[i]['id'] == id:
-                print("Employee {} is done with tasks({}/20):".format(
-                    response[i]['name'], k
-                    ))
-                for done_task in ls:
-                    print("\t", done_task)
+    print(f"Employee {employee_name}\
+          is done with tasks({number_of_done_tasks}/{total_number_of_tasks}):")
+    for task in completed_tasks:
+        print(f"\t{task}")
 
 
-if __name__ == "__main__":
-    first_line(argv[1])
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Error: Employee ID not provided.")
+    else:
+        employee_id = sys.argv[1]
+        get_employee_todo_progress(employee_id)
